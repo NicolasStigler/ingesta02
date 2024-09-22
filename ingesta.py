19cad61057f3b19199e540eea9a5a63559745627
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-09-22 00:59:13 -0500

    Update ingesta.py (#1)
    * Update ingesta.py
    
    * Update Dockerfile

--- ingesta.py
+++ ingesta.py
@@ -1,9 +1,27 @@
 import boto3
+import pandas as pd
+import mysql.connector
+
+def mysql_to_csv(host, port, user, password, database, table, ficheroUpload):
+    connection = mysql.connector.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database
+    )
+
+    query = f"SELECT * FROM {table}"
+    df = pd.read_sql(query, connection)
+    df.to_csv(ficheroUpload, index=False)
+
+    connection.close()
 
 ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+nombreBucket = "nsy-output-s3b"
 
-s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+mysql_to_csv('100.27.161.243', '8005', 'root', 'utec', 'tienda', 'fabricantes', ficheroUpload)
+
+res = boto3.client('s3').upload_file(ficheroUpload, nombreBucket, ficheroUpload)
 
 print("Ingesta completada")
